943d01bdc5d8004430549381480eb2224aad18de
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-19 06:41:26 -0300

    Add React entry point files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { store } from './app/store';
-import App from './App';
-import theme from './theme';
 import './index.css';
+import App from './App';
+import { store } from './app/store';
+import theme from './theme';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <BrowserRouter>
         <ThemeProvider theme={theme}>
-          <CssBaseline />
           <App />
         </ThemeProvider>
-      </Router>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
